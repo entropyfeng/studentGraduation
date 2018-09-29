ea454aaa1294fd9e83c7456b5a2d9fc3d1f81486
lxf <1092758202@qq.com>
2018-09-29 21:08:08 +0800

    upload

---
+++ studentGraduation/src/main/java/com/university/graduation/GraduationApplication.java
@@ -0,0 +1,12 @@
+package com.university.graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationApplication.class, args);
+    }
+}
