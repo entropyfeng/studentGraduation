29627d3048703d8b2c0d951eaeab4bdbb3318cf9
racaljk <1948638989@qq.com>
2018-10-02 10:40:38 +0800

    sql revise

--- studentGraduation/src/main/resources/sqls/sys_permission.sql
+++ studentGraduation/src/main/resources/sqls/sys_permission.sql
@@ -1,34 +1,16 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost_3306
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : graduation
-
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
-
-Date: 2018-10-02 10:10:48
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for sys_permission
--- ----------------------------
 DROP TABLE IF EXISTS `sys_permission`;
 CREATE TABLE `sys_permission` (
-  `sys_permission_id` int(11) NOT NULL,
-  `sys_permission_name` varchar(255) COLLATE utf8_bin unique NOT NULL ,
-  `resource_type` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `resource_url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `permission` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '权限字符串',
+  `sys_permission_id` int AUTO_INCREMENT NOT NULL,
+  `sys_permission_name` varchar(255)  unique NOT NULL ,
+  `resource_type` varchar(255)  DEFAULT NULL,
+  `resource_url` varchar(255)  DEFAULT NULL,
+  `permission` varchar(255)  DEFAULT NULL COMMENT '权限字符串',
   `parent_id` int(255) DEFAULT NULL,
-  `parent_ids` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `parent_ids` varchar(255)  DEFAULT NULL,
   `state` int(11) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL ON CURRENT_TIMESTAMP,
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`sys_permission_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- studentGraduation/src/main/resources/sqls/sys_user.sql
+++ studentGraduation/src/main/resources/sqls/sys_user.sql
@@ -1,32 +1,14 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost_3306
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : graduation
-
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
-
-Date: 2018-10-02 10:11:15
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for sys_user
--- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
-  `sys_user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `sys_username` varchar(255) COLLATE utf8_bin unique NOT NULL,
-  `sys_name` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '名称，可以是昵称或真实姓名，不同系统不同定义',
-  `sys_password` varchar(255) COLLATE utf8_bin NOT NULL,
-  `sys_salt` varchar(255) COLLATE utf8_bin NOT NULL COMMENT '加密密码的盐',
+  `sys_user_id`  int AUTO_INCREMENT NOT NULL,
+  `sys_username` varchar(255)  unique NOT NULL,
+  `sys_name` varchar(255)  DEFAULT NULL COMMENT '名称，可以是昵称或真实姓名，不同系统不同定义',
+  `sys_password` varchar(255)  NOT NULL,
+  `sys_salt` varchar(255)  NOT NULL COMMENT '加密密码的盐',
   `sys_state` int(255) NOT NULL COMMENT '用户状态，0：创建未认证：1：正常：2：用户被锁定',
-  `create_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL ON CURRENT_TIMESTAMP,
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`sys_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
